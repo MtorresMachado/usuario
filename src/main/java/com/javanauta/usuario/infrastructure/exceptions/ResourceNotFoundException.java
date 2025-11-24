7520059e2862a05c1130f8f4c4b9a988b6eee02b
Matheus Torres <torredmach@gmail.com>
2025-11-24 12:48:13 -0300

    adicionados métodos get, delete e login

--- src/main/java/com/javanauta/usuario/infrastructure/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/javanauta/usuario/infrastructure/exceptions/ResourceNotFoundException.java
@@ -1,4 +1,4 @@
-package com.javanauta.aprendendospring.infrastructure.exceptions;
+package com.javanauta.usuario.infrastructure.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
 
